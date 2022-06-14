83e07f3d60bc4690416ef3feb512ee2082be6a84
channurichinmayee <channurichinmayee@gmail.com>
2022-06-15 00:17:30 +0530

    Initial Commit

---
+++ andaaz1.0/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  andaaz1.0
+//
+//  Created by Abhijay Channuri Rao on 13/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logo: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+
+    }
+
+    @IBAction func homeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func aboutButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func feedButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func quizButton(_ sender: UIButton) {
+    }
+    
+
+}
+
