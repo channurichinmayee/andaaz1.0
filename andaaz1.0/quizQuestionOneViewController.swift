66eab97bc789dc0d10ff61b02f63063f656f2245
channurichinmayee <channurichinmayee@gmail.com>
2022-06-17 00:42:01 +0530

    ADDED ALL STORYBOARDS FOR THE QUIZ

---
+++ andaaz1.0/quizQuestionOneViewController.swift
@@ -0,0 +1,43 @@
+//
+//  quizQuestionOneViewController.swift
+//  andaaz1.0
+//
+//  Created by Abhijay Channuri Rao on 17/06/22.
+//
+
+import UIKit
+
+class quizQuestionOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    @IBAction func optionA(_ sender: Any) {
+    }
+    
+    
+    @IBAction func optionB(_ sender: Any) {
+    }
+    
+    @IBAction func optionC(_ sender: Any) {
+    }
+    
+    @IBAction func optionD(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
