208858ab91f659b00725113f629bda47d34d1868
Misha <mitho.oberoi@gmail.com>
2022-06-16 21:18:41 +0530

    lol

---
+++ andaaz1.0/QUIZ1.swift
@@ -0,0 +1,29 @@
+//
+//  QUIZ1.swift
+//  andaaz1.0
+//
+//  Created by sns on 16/06/22.
+//
+
+import UIKit
+
+class QUIZ1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ andaaz1.0/scroll.swift
@@ -0,0 +1,33 @@
+//
+//  scroll.swift
+//  andaaz1.0
+//
+//  Created by sns on 16/06/22.
+//
+
+import UIKit
+
+class scroll: UIViewController {
+
+  
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "About: Do you know how much our wadrobe costs the environment? Every year the fashion industry uses 93 billion cubic meters of water — enough to meet the consumption needs of five million people. Around 20 % of wastewater worldwide comes from fabric dyeing and treatment. Of the total fiber input used for clothing, 87 % is incinerated or disposed of in a landfill. Every year a half a million tons of plastic microfibers are dumped into the ocean, the equivalent of 50 billion plastic bottles. The danger? Microfibers cannot be extracted from the water and they can spread throughout the food chain. Andaaz was created by four high-schoolers Akshaya, Chinmayee, Misha and Usraat, who are passionate about sustainability. They wanted to harness the power of innovation and fashion to come up with a solution to tackle the issues of fast fashion. They believe that the fashion industry’s operating model is exacerbating the problem by stepping up the pace of design and production. Collection launches are no longer seasonal; the replacement of clothing inventories has become much more frequent. This is why Andaaz— an app that makes you rethink about your consumption and provide a one step solution for a sustainable wardrobe. Andaaz’s mission is to make our users rethink about the way we consume."
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
